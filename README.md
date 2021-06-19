d35fd63248a55ee5111f19fa7d31f48bb13d06a5
Abhinav Tushar <abhinav@lepisma.xyz>
2021-06-19 19:42:38 +0530

    add mouseevent

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pride
+# pride :rainbow_flag:
+
+Simple pride top ribbon like below:
+
+![](./screen.png)
+
+Use the following to add in your website.
+
+```html
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/Vernacular-ai/pride@main/index.js"></script>
+```
--- index.js
+++ index.js
@@ -7,14 +7,22 @@ document.addEventListener('DOMContentLoaded', function (event) {
   container.style.left = 0
   container.style.width = '100%'
   container.style['z-index'] = '5050'
+  container.style.transition = 'opacity 0.5s'
 
   for (let color of colors) {
     let ribbon = document.createElement('div')
     ribbon.style['background-color'] = color
-    ribbon.style.opacity = '0.8'
     ribbon.style.height = '3px'
     container.appendChild(ribbon)
   }
+
+  container.addEventListener('mouseenter', e => {
+    container.style.opacity = 0
+  })
+
+  container.addEventListener('mouseleave', e => {
+    container.style.opacity = 1
+  })
 
   document.body.appendChild(container)
 })
